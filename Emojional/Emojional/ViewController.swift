40365eaccb4c42f477df3a64ed5cec27910d83ef
Apple <apple@Apples-MacBook-Pro-6.local>
2019-07-26 07:53:22 -0700

    Complete Project

--- Emojional/Emojional/ViewController.swift
+++ Emojional/Emojional/ViewController.swift
@@ -19,8 +19,10 @@ class ViewController: UIViewController {
 
     @IBAction func showMessage(sender: UIButton) {
         let randomize = Int.random(in: 0 ..< 3)
-        let emojis = ["🍕" : "pizza", "🥞" : "pancakes", "🍟" : "fries"]
-        let customMessage = ["pizza" : ["Cheesus Crust", "Have a slice day", "Pizzachu! I cheese you!"], "pancakes" : ["No one stacks up to you", "You're flippen' amazing!", "The thing about pancake jokes is that they always fall flat"], "fries" : ["Cookin' so fry", "Time fries when I'm with you", "It's FRYday!"]]
+        let emojis = ["🍕" : "pizza", "🥞" : "pancakes", "🍟" : "fries", "🌮" : "taco", "🍿" : "popcorn", "🍔" : "burger"]
+        
+        let customMessage = ["pizza" : ["Cheesus Crust", "Have a slice day", "Pizzachu! I cheese you!"], "pancakes" : ["No one stacks up to you", "You're flippen' amazing!", "The thing about pancake jokes is that they always fall flat"], "fries" : ["Cookin' so fry", "Time fries when I'm with you", "It's FRYday!"], "taco" : ["Have a specTACOlar day!", "Taco 'bout awesome!", "I have fillings too!"], "popcorn" : ["Hope your day is poppin'", "Just poppin' in to say that you are fabulous", "Why aren't there many jokes about popcorn? Because they're corny"], "burger" : ["You hamBURGLED my heart", "Say double cheese!", "Did you hear about the hamburger that couldn't stop making jokes? It was on a roll"]]
+        
         let selectedEmotion = sender.titleLabel?.text
         let emojiMessage = customMessage[emojis[selectedEmotion!]!]?[randomize]
         
