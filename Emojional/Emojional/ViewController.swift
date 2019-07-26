67c214745bdb7dd954115f506a3cb61b5328475d
Apple <apple@Apples-MacBook-Pro-6.local>
2019-07-26 06:53:40 -0700

    Complete Iteration 2

--- Emojional/Emojional/ViewController.swift
+++ Emojional/Emojional/ViewController.swift
@@ -18,9 +18,16 @@ class ViewController: UIViewController {
 
 
     @IBAction func showMessage(sender: UIButton) {
-                let alertController = UIAlertController()
+        let randomize = Int.random(in: 0 ..< 3)
+        let emojis = ["🍕" : "pizza", "🥞" : "pancakes", "🍟" : "fries"]
+        let customMessage = ["pizza" : ["Cheesus Crust", "Have a slice day", "Pizzachu! I cheese you!"], "pancakes" : ["No one stacks up to you", "You're flippen' amazing!", "The thing about pancake jokes is that they always fall flat"], "fries" : ["Cookin' so fry", "Time fries when I'm with you", "It's FRYday!"]]
+        let selectedEmotion = sender.titleLabel?.text
+        let emojiMessage = customMessage[emojis[selectedEmotion!]!]?[randomize]
+        
+        let alertController = UIAlertController(title: "Emojional", message: emojiMessage , preferredStyle: UIAlertController.Style.alert)
                 alertController.addAction(UIAlertAction(title :"Ok", style : UIAlertAction.Style.default, handler : nil))
                 present(alertController, animated :true, completion : nil)
+    
     }
 
 
